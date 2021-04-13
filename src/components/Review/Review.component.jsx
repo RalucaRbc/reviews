3c01f10f090ba0e1348b41c04373fc818987a990
RalucaRbc <office.movmil@gmail.com>
2021-04-13 10:44:01 +0100

    Make both chevrons work; add validation for index<0 || index>length of people array

--- src/components/Review/Review.component.jsx
+++ src/components/Review/Review.component.jsx
@@ -5,6 +5,31 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 export const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
   return (
     <article className="review">
       <div className="img-container">
@@ -18,10 +43,10 @@ export const Review = () => {
       <p className="info">{text}</p>
       <div className="button-container">
         <button>
-          <FaChevronLeft className="prev-btn" />
+          <FaChevronLeft className="prev-btn" onClick={prevPerson} />
         </button>
         <button>
-          <FaChevronRight className="next-btn" />
+          <FaChevronRight className="next-btn" onClick={nextPerson} />
         </button>
       </div>
       <button className="random-btn">Surprise me</button>
